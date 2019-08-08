df2d55eceebb381d9d11ee70b4067d4aad1bcf9f
overlordx88 <47886384+overlordx88@users.noreply.github.com>
2019-08-08 04:37:43 +0430

    Add files via upload

--- 003.js
+++ 003.js
@@ -1,5 +1,5 @@
 var uname = ['Zebra', 'Tiger', 'Lion', 'Bird', 'Cat', 'Dog'];
-var user = propmt ("Enter your username?");
+var user = Number(prompt ("Enter your username?"));
 var stack = uname.indexOf(user);
 var result = (stack == -1) ? alert(" ACCESS DENIED " + user) : alert(  "Welcome " + user);
 console.log(uname.indexOf(user));
